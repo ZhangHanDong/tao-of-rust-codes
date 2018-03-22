f2ffcace3ea1a9437d8febd3f2b7c184900036d7
blackanger <blackanger.z@gmail.com>
2018-03-22 16:57:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 《Rust编程之道》源码示例
+
+安装：
+
+```
+cargo install tao-of-rust
+
+```
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+//! 《Rust编程之道》
+//!
+//!  这里记录本书中涉及的所有示例代码。
+
+//! Rust安装的所有细节可以在 [附录A][appendix] 中找到。
+//!
+//! 正文从 [第一章: 新时代的语言][ch1] 开始。
+//!
+//! [appendix]: /src/tao_of_rust/appendix/mod.rs.html
+//! [ch1]: /src/tao_of_rust/ch1/mod.rs.html
+
+
+pub mod ch1;
+pub mod ch2;
+pub mod ch3;
+pub mod ch4;
+pub mod ch5;
+pub mod ch6;
+pub mod ch7;
+pub mod ch8;
+pub mod ch9;
+pub mod ch10;
+pub mod ch11;
+pub mod ch12;
+pub mod ch13;
+
+pub mod appendix;
