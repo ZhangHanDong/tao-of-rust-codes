91ce14d1d55b9bad4b0f1bdcf5f64b67a11152b8
laizy <laizhichao@onchain.com>
2018-10-11 14:35:02 +0800

    remove unsafe code in AlmostReady future

--- src/ch11/futures-demo/src/main.rs
+++ src/ch11/futures-demo/src/main.rs
@@ -4,10 +4,7 @@
 //     executor::{ThreadPool,LocalPool},
 //     task::{SpawnExt, LocalSpawnExt},
 // };
-use futures::{
-    executor::ThreadPool,
-    task::SpawnExt,
-};
+use futures::executor::ThreadPool;
 use std::future::{Future};
 use std::pin::Pin;
 use std::task::*;
@@ -25,13 +22,13 @@ pub fn almost_ready(value: i32) -> AlmostReady {
 
 impl Future for AlmostReady {
     type Output = i32;
-    fn poll(self: Pin<&mut Self>, lw: &LocalWaker)
+    fn poll(mut self: Pin<&mut Self>, lw: &LocalWaker)
         -> Poll<Self::Output>
     {
         if self.ready {
             Poll::Ready(self.value + 1)
         } else {
-            unsafe { Pin::get_mut_unchecked(self).ready = true ;}
+            self.ready = true;
             lw.wake();
             Poll::Pending
         }
