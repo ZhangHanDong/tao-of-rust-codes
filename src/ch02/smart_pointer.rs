89e8f1065be425655cdbad79a7a58b50790ac2c1
blackanger <blackanger.z@gmail.com>
2019-01-21 21:59:59 +0800

    modify ch02 smart pointer

--- src/ch02/smart_pointer.rs
+++ src/ch02/smart_pointer.rs
@@ -4,14 +4,14 @@
 ///
 /// ```
 /// fn box_demo(){
-///     #[derive(PartialEq)]
+///     #[derive(PartialEq, Debug)]
 ///     struct Point {
 ///       x: f64,
 ///       y: f64,
 ///     }
 ///     // 将Point实例装箱（放到堆内存）
 ///     let box_point = Box::new(Point { x: 0.0, y: 0.0 });
-///     let unboxed_point: Point = *boxed_point; // 通过解引用操作符取出Point实例
+///     let unboxed_point: Point = *box_point; // 通过解引用操作符取出Point实例
 ///     assert_eq!(unboxed_point, Point { x: 0.0, y: 0.0 });
 /// }
 /// box_demo();
