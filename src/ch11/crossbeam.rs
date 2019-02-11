398e3667cd782163e8c61491ed15bc6c8b38db11
blackanger <blackanger.z@gmail.com>
2019-02-11 20:27:45 +0800

    modify ch11/crossbeam

--- src/ch11/crossbeam.rs
+++ src/ch11/crossbeam.rs
@@ -46,10 +46,10 @@
 /// Basic usage: 使用crossbeam的scoped thread
 ///
 /// 请用 Rust 2018 Edtion
-/// 
+///
 /// ```rust
 /// use crossbeam::thread::scope;
-/// 
+///
 /// let array = [1, 2, 3];
 ///
 /// scope(|scope| {
@@ -60,17 +60,61 @@
 ///     }
 /// });
 /// ```
+/// Basic usage: 使用crossbeam channel 「0.7.1」
 ///
-/// Basic usage: 使用crossbeam channel
+/// select!宏语法变更
 ///
 /// 请用 Rust 2018 Edtion
-/// 
+///
 /// ```rust
 /// use crossbeam_channel::select;
 /// use crossbeam_channel as channel;
-/// 
+///
 /// use std::thread;
-/// 
+///
+/// fn fibonacci(fib: channel::Sender<u64>, quit: channel::Receiver<()>) {
+///     let (mut x, mut y) = (0, 1);
+///     loop {
+///         select! {
+///             send(fib, x) -> _ => {
+///                 let tmp = x;
+///                 x = y;
+///                 y = tmp + y;
+///             }
+///             recv(quit) -> _ => {
+///                 println!("quit");
+///                 return;
+///             }
+///         }
+///     }
+/// }
+///
+/// fn main() {
+///     let (fib_s, fib_r) = channel::bounded(0);
+///     let (quit_s, quit_r) = channel::bounded(0);
+///
+///     thread::spawn(move || {
+///         for _ in 0..10 {
+///             println!("{}", fib_r.recv().unwrap());
+///         }
+///         quit_s.send(());
+///     });
+///
+///     fibonacci(fib_s, quit_r);
+/// }
+/// ```
+///
+///
+/// Basic usage: 使用crossbeam channel 「已过期」
+///
+/// 请用 Rust 2018 Edtion
+///
+/// ```rust
+/// use crossbeam_channel::select;
+/// use crossbeam_channel as channel;
+///
+/// use std::thread;
+///
 /// fn fibonacci(fib: channel::Sender<u64>, quit: channel::Receiver<()>) {
 ///     let (mut x, mut y) = (0, 1);
 ///     loop {
@@ -87,18 +131,18 @@
 ///         }
 ///     }
 /// }
-/// 
+///
 /// fn main() {
 ///     let (fib_s, fib_r) = channel::bounded(0);
 ///     let (quit_s, quit_r) = channel::bounded(0);
-/// 
+///
 ///     thread::spawn(move || {
 ///         for _ in 0..10 {
 ///             println!("{}", fib_r.recv().unwrap());
 ///         }
 ///         quit_s.send(());
 ///     });
-/// 
+///
 ///     fibonacci(fib_s, quit_r);
 /// }
 /// ```
@@ -106,19 +150,19 @@
 /// Basic usage: 使用crossbeam channel，通过引用共享
 ///
 /// 请用 Rust 2018 Edtion
-/// 
+///
 /// ```rust
 /// use crossbeam::channel as channel;
 /// fn main(){
 /// let (s, r) = channel::unbounded();
-/// 
+///
 /// crossbeam::scope(|scope| {
 ///     // Spawn a thread that sends one message and then receives one.
 ///     scope.spawn(|| {
 ///         s.send(1);
 ///         r.recv().unwrap();
 ///     });
-/// 
+///
 ///     // Spawn another thread that does the same thing.
 ///     scope.spawn(|| {
 ///         s.send(2);
@@ -131,21 +175,21 @@
 /// Basic usage: 使用crossbeam channel，通过clone共享
 ///
 /// 请用 Rust 2018 Edtion
-/// 
+///
 /// ```rust
 /// use std::thread;
 /// use crossbeam::channel as channel;
-/// 
+///
 /// fn main(){
 /// let (s1, r1) = channel::unbounded();
 /// let (s2, r2) = (s1.clone(), r1.clone());
-/// 
+///
 /// // Spawn a thread that sends one message and then receives one.
 /// thread::spawn(move || {
 ///     s1.send(1);
 ///     r1.recv().unwrap();
 /// });
-/// 
+///
 /// // Spawn another thread that receives a message and then sends one.
 /// thread::spawn(move || {
 ///     r2.recv().unwrap();
@@ -153,6 +197,6 @@
 /// });
 /// }
 /// ```
-pub fn crossbeam_demo(){
+pub fn crossbeam_demo() {
     unimplemented!();
 }
